ebaac8d440b3f75cb3f7c90b0dd68169f17b0dfc
Quentin <pedago@pcalb-eb0112.univ-jfc.fr>
2018-12-04 16:24:52 +0100

    Fonction d'ajout de commande dans DAO

--- src/main/java/modele/DAO.java
+++ src/main/java/modele/DAO.java
@@ -18,66 +18,99 @@ import javax.sql.DataSource;
  * @author Quentin
  */
 public class DAO {
-    
-    private final DataSource myDataSource;
-	
-    /**    
-	 * @param dataSource la source de données à utiliser
-	 */
-	
-        
-        public DAO(DataSource dataSource) {
-		this.myDataSource = dataSource;
-	}
-    
-    
 
- public boolean verifClientConnexion(String email,String id) throws SQLException {
-            boolean verif = false;
-            String sql = "SELECT COUNT(*) AS Nombre FROM CUSTOMER WHERE EMAIL=? AND CUSTOMER_ID=? ";
-            try (Connection connection = myDataSource.getConnection();
-                    PreparedStatement stmt = connection.prepareStatement(sql)){
-                    
-                    stmt.setString(1,email);
-                    stmt.setString(2,id);
-                    
-                    try(ResultSet resultSet = stmt.executeQuery()){
-                        if(resultSet.next()){
-                            verif = resultSet.getInt("Nombre")==1;
-                        }
-                    }catch (SQLException ex) {
-			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
-			throw new SQLException(ex.getMessage());
+    private final DataSource myDataSource;
+
+    /**
+     * @param dataSource la source de données à utiliser
+     */
+    public DAO(DataSource dataSource) {
+        this.myDataSource = dataSource;
+    }
+
+    public boolean verifClientConnexion(String email, String id) throws SQLException {
+        boolean verif = false;
+        String sql = "SELECT COUNT(*) AS Nombre FROM CUSTOMER WHERE EMAIL=? AND CUSTOMER_ID=? ";
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, id);
+
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                if (resultSet.next()) {
+                    verif = resultSet.getInt("Nombre") == 1;
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+                throw new SQLException(ex.getMessage());
             }
             return verif;
         }
-}
-        /**
-         * Renvoie le nom du client en fonction de son email et son id
-         * @param email
-         * @param id
-         * @return
-         * @throws SQLException 
-         */
-        public String nomClient(String email,String id) throws SQLException{
-            String result = "";
-            String sql = "SELECT NAME FROM CUSTOMER WHERE EMAIL=? AND CUSTOMER_ID=? ";
-            
-            try (Connection connection = myDataSource.getConnection();
-                    PreparedStatement stmt = connection.prepareStatement(sql)){
-                    
-                    stmt.setString(1,email);
-                    stmt.setString(2,id);
-                    
-                    try(ResultSet resultSet = stmt.executeQuery()){
-                        if(resultSet.next()){
-                            result = resultSet.getString("NAME");
-                        }
-            }catch (SQLException ex) {
-			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
-			throw new SQLException(ex.getMessage());
-                    }
+    }
+
+    /**
+     * Renvoie le nom du client en fonction de son email et son id
+     *
+     * @param email
+     * @param id
+     * @return
+     * @throws SQLException
+     */
+    public String nomClient(String email, String id) throws SQLException {
+        String result = "";
+        String sql = "SELECT NAME FROM CUSTOMER WHERE EMAIL=? AND CUSTOMER_ID=? ";
+
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, id);
+
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                if (resultSet.next()) {
+                    result = resultSet.getString("NAME");
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+                throw new SQLException(ex.getMessage());
+            }
             return result;
+        }
+    }
+
+    /**
+     * Ajoute une commande à la table Purchase_order
+     *
+     * @param commande
+     * @throws SQLException
+     */
+    public void ajoutCommande(PurchaseEntity commande) throws SQLException {
+
+        String sql = "SELECT MAX(order_num)+1 FROM PURCHASE_ORDER";
+        int newId = -1;
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    newId = rs.getInt("ORDER_NUM");
+                }
             }
         }
+        String sql2 = "INSERT INTO PURCHASE_ORDER VALUES(?,?,?,?,?,?,?)";
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setInt(1, newId);
+            stmt.setInt(2, commande.getQuantite());
+            stmt.setFloat(3, commande.getPrix());
+            stmt.setFloat(4, commande.getFraisport());
+            stmt.setString(5, commande.getDateachat());
+            stmt.setString(6, commande.getDatelivraison());
+            stmt.setString(7, commande.getDescription());
+
+            stmt.executeUpdate();
+        }
+    }
 }
