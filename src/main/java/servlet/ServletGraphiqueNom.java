5b771413836f1ff7fdf6f4c220c5c748bd95cee3
Drisnor <floflu19@gmail.com>
2018-12-18 09:19:01 +0100

    Création d'une deuxième servlet pour les graphiques (partie nom)
    TODO :
    - Finir la redirection pour l'inscription
    - Déployer BDD
    - Implémenter sessions
    - Implementer les graphiques

--- src/main/java/servlet/ServletGraphiqueNom.java
+++ src/main/java/servlet/ServletGraphiqueNom.java
@@ -5,12 +5,20 @@
  */
 package servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modele.*;
 
 /**
  *
@@ -28,20 +36,29 @@ public class ServletGraphiqueNom extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletGraphiqueNom</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletGraphiqueNom at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+            throws ServletException, IOException, Exception {
+            DAO dao = new DAO(DataSourceFactory.getDataSource());
+		// Properties est une Map<clé, valeur> pratique pour générer du JSON
+		Properties resultat = new Properties();
+		try {
+                        String dateDebut = request.getParameter("dateDebut");
+                        String datefin = request.getParameter("datefin");
+                        resultat.put("records", dao.TurnoverClient(dateDebut,datefin));  // TODO Comme pour la ServletGraphiqueNom, Implémenter TurnoverClient DAO
+		} catch (SQLException ex) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resultat.put("records", Collections.EMPTY_LIST);
+			resultat.put("message", ex.getMessage());
+		}
+                try (PrintWriter out = response.getWriter()) {
+			// On spécifie que la servlet va générer du JSON
+			response.setContentType("application/json;charset=UTF-8");
+			// Générer du JSON
+			// Gson gson = new Gson();
+			// setPrettyPrinting pour que le JSON généré soit plus lisible
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			String gsonData = gson.toJson(resultat);
+			out.println(gsonData);
+		}
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -56,7 +73,11 @@ public class ServletGraphiqueNom extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(ServletGraphiqueNom.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +91,11 @@ public class ServletGraphiqueNom extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(ServletGraphiqueNom.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
